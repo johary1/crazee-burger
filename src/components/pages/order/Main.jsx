37eace313792ade5642adb7b77808008cd0579c2
johary1 <joharisoa@gmail.com>
2023-10-31 15:58:30 +0100

    🚧 F04 : Order> Create layout

---
+++ src/components/pages/order/Main.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default function Main() {
+  return <MainStyled>Main</MainStyled>;
+}
+
+const MainStyled = styled.div`
+  background-color: lightblue;
+  border: 1px solid black;
+  padding: 10px;
+  flex: 1;
+`;
