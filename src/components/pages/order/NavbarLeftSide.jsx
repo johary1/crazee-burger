1d2a75bc819b3c55528483542e4a5a0e81028bc5
johary1 <joharisoa@gmail.com>
2023-11-03 16:07:41 +0100

    💄 F04 : Order > Navbar > update style and add switch admin button

--- src/components/pages/order/NavbarLeftSide.jsx
+++ src/components/pages/order/NavbarLeftSide.jsx
@@ -1,15 +1,11 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 import LogoBurger from "../../reusable-ui/LogoBurger";
 export default function NavbarLeftSide() {
   return (
-    <NavbarLeftSideStyled>
-      <LogoBurger />
-    </NavbarLeftSideStyled>
+    // <NavbarLeftSideStyled>
+    <LogoBurger />
+    // </NavbarLeftSideStyled>
   );
 }
 
-const NavbarLeftSideStyled = styled.div`
-  /* transform: scale(0.4);
-  margin-left: -10%;
-  margin-bottom: 15px; */
-`;
+// const NavbarLeftSideStyled = styled.div``;
--- src/components/pages/order/NavbarRightSide.jsx
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -7,12 +7,13 @@ import { PiUserCircleFill } from "react-icons/pi";
 export default function NavbarRightSide({ firstName }) {
   return (
     <NavbarRightSideStyled>
+      {/* <div className="admin-button">Activer le mode admin</div> */}
       <NavProfileStyled>
         <h1>
-          Hey <span>{firstName}</span>
+          Hey, <span>{firstName}</span>
         </h1>
         <Link to="/">
-          <button>Se déconnecter</button>
+          <small>Se déconnecter</small>
         </Link>
       </NavProfileStyled>
       <NavProfileIconStyled>
@@ -27,24 +28,38 @@ const NavbarRightSideStyled = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  margin-right: 70px;
-  padding-top: 20px auto 20px;
+  margin-right: 50px;
+
+  /* .admin-button {
+    background-color: orange;
+    border-radius: ${theme.borderRadius.extraRound};
+    border: 1px solid ${theme.colors.primary};
+    font-size: 12px;
+    padding: 8px;
+    width: 170px;
+  } */
 `;
 
 const NavProfileStyled = styled.div`
   h1 {
     font-size: 24px;
-    color: ${theme.colors.primary};
+    font-weight: 300;
+    color: ${theme.colors.greySemiDark};
     span {
-      color: ${theme.colors.greySemiDark};
+      color: ${theme.colors.primary};
+      font-weight: 600;
     }
   }
 
-  button {
-    border: none;
-    background-color: white;
-    color: ${theme.colors.greyMedium};
+  small {
+    text-decoration: none;
+    font-size: ${theme.fonts.XXS};
+    color: ${theme.colors.greyBlue};
     cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+      color: ${theme.colors.greyDark};
+    }
   }
 `;
 
