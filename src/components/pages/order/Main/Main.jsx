34e8f4ef9218cab7fe4f17b5861ce429bae731ab
johary1 <joharisoa@gmail.com>
2023-12-06 16:16:25 +0100

    🚧 F07 : Add layout and style Admin

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme";
 import Menu from "./Menu";
+import Admin from "./Admin/Admin";
 
 export default function Main() {
   return (
     <MainStyled>
       <div className="basket">Basket area</div>
-      <Menu />
+      <div className="menu-and-admin">
+        <Menu />
+        <Admin />
+      </div>
     </MainStyled>
   );
 }
@@ -14,15 +18,21 @@ export default function Main() {
 const MainStyled = styled.div`
   background: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-  border-radius: 0px 0px 15px 15px;
-  padding: 20px;
+  border-radius: 15px;
+
   flex: 1;
-  /* height: 80%; */
+  height: calc(95vh - 10vh);
   display: grid;
-  grid-template-columns: 15% 1fr;
-  overflow-y: scroll;
+  grid-template-columns: 25% 1fr;
 
   .basket {
     background: green;
+    border-bottom-left-radius: 15px;
+  }
+
+  .menu-and-admin {
+    position: relative;
+    overflow-y: hidden;
+    display: grid;
   }
 `;
--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -26,10 +26,14 @@ export default function Menu() {
 
 const MenuStyled = styled.div`
   background: ${theme.colors.background_white};
+  height: 85vh;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
   /* box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset; */
+  overflow-y: scroll;
+  border-bottom-right-radius: 15px;
 `;
