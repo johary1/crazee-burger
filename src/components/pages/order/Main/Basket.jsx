450d0624c28430f623d6576c7ecbe8422d4e43b8
johary1 <joharisoa@gmail.com>
2024-06-17 21:19:49 +0200

    💄 F10: Basket > add layout

--- src/components/pages/order/Main/Basket.jsx
+++ src/components/pages/order/Main/Basket.jsx
@@ -2,10 +2,28 @@ import styled from "styled-components";
 import { theme } from "../../../../theme";
 
 export default function Basket() {
-  return <BasketStyled>Basket</BasketStyled>;
+  return (
+    <BasketStyled>
+      <div className="head">Header</div>
+      <div className="body">Body</div>
+      <div className="footer">Footer</div>
+    </BasketStyled>
+  );
 }
 
 const BasketStyled = styled.div`
   background: salmon;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  display: flex;
+  flex-direction: column;
+
+  .head {
+    background: ${theme.colors.background_dark};
+  }
+  .body {
+    background: ${theme.colors.background_white};
+  }
+  .footer {
+    background: ${theme.colors.background_dark};
+  }
 `;
